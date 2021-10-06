f60f20edf2e025062a0bc68272e64954a9e40b37
Francisco Louro <francisco.louro16@hotmail.com>
2021-10-06 12:26:00 +0100

    Added new features (Issues#2)

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = Flask(__name__, template_folder='html')
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, './temp')
 ALLOWED_EXTENSIONS = {'json'}
 app.config.update(TEMPLATES_AUTO_RELOAD = True)
-
 yaml = functions.readConfig()
 configapp = yaml['app']
 configzabbix = yaml['zabbix']
@@ -16,6 +15,7 @@ configemail = yaml['email']
 
 zabbixauth = functions.authentication(configzabbix['server'], configzabbix['credentials'])
 if configapp['env'] != 'dev':
+    app.config['DEBUG'] = True
     app.config['BASIC_AUTH_USERNAME'] = configapp['credentials']['username']
     app.config['BASIC_AUTH_PASSWORD'] = configapp['credentials']['password']
     app.config['BASIC_AUTH_FORCE'] = True
@@ -34,14 +34,14 @@ def upload():
         with open(f"{app.config['UPLOAD_FOLDER']}/{filename}", "r") as File:
             var = json.load(File)
             functions.changeHostFile(var)
-        with open('data.json', 'r+') as File:
+        """with open('data.json', 'r+') as File:
             res = functions.runchecker()
             file_data = json.load(File)
             file_data.update(res)
             
             File.seek(0)
-            json.dump(file_data, File, indent = 4)
-        return redirect("/")
+            json.dump(file_data, File, indent = 4)"""
+    return redirect("/")
 
 @app.route("/api/v1/runchecker", methods=['POST'])
 def runchecker():
@@ -49,10 +49,8 @@ def runchecker():
 
 @app.route('/api/v1/sendemail', methods=['POST'])
 def sendemail():
-    data = request.form
-    host = data['host']
-    daysleft = data['daysleft']
-    return functions.sendEmail(configemail['receiver'], configemail['sender'], host, daysleft)
+    data = request.json
+    return functions.sendEmail(configemail['receiver'], configemail['sender'], data)
 
 @app.route("/api/v1/addhost", methods=['POST'])
 def addHost():
@@ -65,11 +63,35 @@ def addHost():
             return "Success"
     else:
         return "Wrong domain format"
-
+# TO BE WORKED
 @app.route("/v1/api/gethostfile")
 def gethostfile():
-    uploads = os.path.join(app.root_path, '.')
-    return send_from_directory(uploads, 'hosts.json', as_attachment=True)
+    uploads = os.path.join(app.root_path, './temp')
+    with open('temp/hosts.json', 'w') as file:
+        temp = {}
+        temp['hosts'] = []
+        data = functions.getData()
+        for host in data:
+            temp['hosts'].append(host)
+        json.dump(temp, file, indent = 4)
+        file.seek(0)
+        return send_from_directory(uploads, 'hosts.json', as_attachment=True)
+
+@app.route("/api/v1/gethosts")
+def getHosts():
+    host = request.args.get('host')
+    if host == None:
+        return functions.getHosts()
+    else:
+        return functions.getHosts(host)
+
+@app.route("/api/v1/getdata")
+def getData():
+    host = request.args.get('host')
+    if host == None:
+        return functions.getData()
+    else:
+        return functions.getData(host)
 
 @app.route("/api/v1/delhost", methods=['POST'])
 def delHost():
@@ -83,9 +105,15 @@ def delHost():
     else:
         return "Wrong domain format"
 
+
 @app.route("/")
 def root():
-    return render_template('index.html', hosts = functions.getHosts()['hosts'], data = functions.getData())
+    data = functions.getData()
+
+    data.sort(key = lambda x:x["daystoexpire"])
+    
+
+    return render_template('index.html', data = data)
 
 @app.route("/newhosts")
 def newhost():
